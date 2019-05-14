a73160357ea13c1fe8e34e86aba4ad997c5a9ace
Galiov <galilea.valle@gmail.com>
2019-05-14 09:41:42 -0500

    Se creó archivo que contiene los principales apartados del menú

---
+++ src/Components/Menu.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Items from './Items';
+import './Menu.css';
+//import HamburgerType from './SubMenu';
+
+const hamburgers = () => {
+  window.location.href = "https://www.google.com/search?tbm=isch&sa=1&ei=MXfaXKyTCIT-sAWMwJRY&q=hamburger&oq=hamburger&gs";
+	/*return(
+		<div className="hamburgers">
+			<HamburgerType/>
+		</div>	
+		);
+	*/
+};
+
+
+const PrincipalItems = () => {
+
+	return (
+	    <div className="menu">
+          <Items name="Hamburguesas" action={hamburgers}/>
+          <Items name="Acompañamientos"/>
+          <Items name="Bebidas"/>
+        </div>
+        );
+};
+
+export default PrincipalItems;
